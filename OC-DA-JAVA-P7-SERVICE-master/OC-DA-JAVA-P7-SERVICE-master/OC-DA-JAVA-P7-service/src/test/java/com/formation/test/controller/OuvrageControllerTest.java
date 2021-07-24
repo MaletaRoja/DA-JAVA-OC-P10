df635775292a734ec52f3ee314574341ad09c0d2
Michel <kintelalpz@gmail.com>
2021-07-24 17:41:15 +0200

    Implémentation test pour la méthode setReservations()

--- OC-DA-JAVA-P7-SERVICE-master/OC-DA-JAVA-P7-SERVICE-master/OC-DA-JAVA-P7-service/src/test/java/com/formation/test/controller/OuvrageControllerTest.java
+++ OC-DA-JAVA-P7-SERVICE-master/OC-DA-JAVA-P7-SERVICE-master/OC-DA-JAVA-P7-service/src/test/java/com/formation/test/controller/OuvrageControllerTest.java
@@ -35,20 +35,7 @@ import io.jsonwebtoken.lang.Assert;
 @ExtendWith(MockitoExtension.class)
 public class OuvrageControllerTest {
 	
-	/*
-	@InjectMocks
-	private OuvragesController ouvragesController_mock;
 	
-	
-	@Mock
-	OuvrageService ouvrageService;
-	
-	@Mock
-	EmpruntService empruntService;
-	*/
-	
-
-	//@InjectMocks
 	private OuvragesController ouvragesController;
 	
 	@Mock
@@ -190,93 +177,6 @@ public class OuvrageControllerTest {
 	@Test
 	public void estEmprunteTest() {
 
-		/*
-		OuvragesController ouvragesController = new OuvragesController();
-
-		List<Emprunt> emprunts_u1 = new ArrayList<>();
-		List<Emprunt> emprunts_u2 = new ArrayList<>();
-		List<Emprunt> emprunts_u3 = new ArrayList<>();
-
-		List<Emprunt> empruntsActifsU1 = new ArrayList<>();
-
-		Utilisateur u1 = new Utilisateur(101, "u1_nom", "u1_prenom", "email1", "u1_pass", false, "USER", emprunts_u1);
-		Utilisateur u2 = new Utilisateur(102, "u2_nom", "u2_prenom", "email2", "u2_pass", false, "USER", emprunts_u2);
-		Utilisateur u3 = new Utilisateur(103, "u1_nom", "u3_prenom", "email3", "u3_pass", false, "USER", emprunts_u3);
-
-		List<Exemplaire> exemplaires_o1 = new ArrayList<>();
-		List<Exemplaire> exemplaires2_o2 = new ArrayList<>();
-		List<Exemplaire> exemplaires3_o3 = new ArrayList<>();
-
-		List<Ouvrage> ouvrages = new ArrayList<>();
-		Ouvrage o1 = new Ouvrage(1, "titre1", "nom1", "prenom1", "edition1", "genre1", exemplaires_o1);
-		Ouvrage o2 = new Ouvrage(2, "titre2", "nom2", "prenom2", "edition2", "genre2", exemplaires2_o2);
-		Ouvrage o3 = new Ouvrage(3, "titre3", "nom3", "prenom3", "edition3", "genre3", exemplaires3_o3);
-
-		ouvrages.add(o1);
-		ouvrages.add(o2);
-		ouvrages.add(o3);
-
-		// Exemplaires de l'ouvrage 1
-
-		Exemplaire ex1_o1 = new Exemplaire(11, o1, emprunts_u1, false, true);
-		Exemplaire ex2_o1 = new Exemplaire(12, o1, emprunts_u2, false, true);
-		Exemplaire ex3_o1 = new Exemplaire(13, o1, emprunts_u3, false, true);
-
-		// Exemplaires de l'ouvrage 2
-
-		Exemplaire ex1_o2 = new Exemplaire(14, o2, emprunts_u1, false, true);
-		Exemplaire ex2_o2 = new Exemplaire(15, o2, emprunts_u2, false, true);
-		Exemplaire ex3_o2 = new Exemplaire(16, o2, emprunts_u3, false, true);
-
-		// Exemplaires de l'ouvrage 3
-
-		Exemplaire ex1_o3 = new Exemplaire(17, o3, emprunts_u1, false, true);
-		Exemplaire ex2_o3 = new Exemplaire(18, o3, emprunts_u2, false, true);
-		Exemplaire ex3_o3 = new Exemplaire(19, o3, emprunts_u3, false, true);
-
-		Emprunt e1_u1 = new Emprunt(21, ex1_o1, u1, LocalDateTime.now(), LocalDateTime.now().plusYears(4), false, true);
-		Emprunt e2_u1 = new Emprunt(22, ex1_o2, u1, LocalDateTime.now(), LocalDateTime.now().plusYears(5), false, true);
-		Emprunt e3_u1 = new Emprunt(23, ex1_o3, u1, LocalDateTime.now(), LocalDateTime.now().plusYears(6), false, true);
-
-		Emprunt e1_u2 = new Emprunt(24, ex2_o1, u2, LocalDateTime.now(), LocalDateTime.now().plusYears(7), false, true);
-		Emprunt e2_u2 = new Emprunt(25, ex2_o2, u2, LocalDateTime.now(), LocalDateTime.now().plusYears(7), false, true);
-		Emprunt e3_u2 = new Emprunt(26, ex2_o3, u2, LocalDateTime.now(), LocalDateTime.now().plusYears(7), false, true);
-
-		Emprunt e1_u3 = new Emprunt(27, ex3_o1, u3, LocalDateTime.now(), LocalDateTime.now().plusYears(7), false, true);
-		Emprunt e2_u3 = new Emprunt(28, ex3_o2, u3, LocalDateTime.now(), LocalDateTime.now().plusYears(7), false, true);
-		Emprunt e3_u3 = new Emprunt(29, ex3_o3, u2, LocalDateTime.now(), LocalDateTime.now().plusYears(7), false, true);
-
-		exemplaires_o1.add(ex1_o1);
-		exemplaires_o1.add(ex2_o1);
-		exemplaires_o1.add(ex3_o1);
-
-		exemplaires2_o2.add(ex1_o2);
-		exemplaires2_o2.add(ex2_o2);
-		exemplaires2_o2.add(ex3_o2);
-
-		exemplaires3_o3.add(ex1_o3);
-		exemplaires3_o3.add(ex2_o3);
-		exemplaires3_o3.add(ex3_o3);
-
-		emprunts_u1.add(e1_u1);
-		// emprunts_u1.add(e2_u1);
-		emprunts_u1.add(e3_u1);
-
-
-		
-		emprunts_u1.add(e1_u1);
-		// emprunts_u1.add(e2_u1);
-		emprunts_u1.add(e3_u1);
-		
-		
-		// Emprunts actifs de l'utilisateur 1
-
-		empruntsActifsU1.add(e1_u1);
-		// empruntsActifsU1.add(e2_u1);
-		empruntsActifsU1.add(e3_u1);
-		
-		*/
-
 		List<OuvrageAux> listeOuvragesAux = ouvragesController.estEmprunte(ouvrages, empruntsActifsU1);
 		System.out.println(
 				"reservable 0? " + listeOuvragesAux.get(0).isReservable() + " id: " + listeOuvragesAux.get(0).getId());
@@ -311,8 +211,7 @@ public class OuvrageControllerTest {
 	}
 
 	@Test 
-	public void setReservationTest() {
-		
+	public void setReservationListNullTest() {
 		
 		List<Ouvrage> ouvrages = new ArrayList<>();
 		o1 = new Ouvrage(1, "titre1", "nom1", "prenom1", "edition1", "genre1", exemplaires1_o1);
@@ -335,14 +234,65 @@ public class OuvrageControllerTest {
 		List<Reservation> reservations_o2 = null;
 		List<Reservation> reservations_o3 = null;
 		
-		//Reservation r1_o1 = new Reservation();
-		
 		when(reservationService.obtenirListeReservationsParOuvrage(1)).thenReturn(reservations_o1);
 		when(reservationService.obtenirListeReservationsParOuvrage(2)).thenReturn(reservations_o2);
 		when(reservationService.obtenirListeReservationsParOuvrage(3)).thenReturn(reservations_o3);
 		ouvragesController.setReservations(listeOuvragesAux);
+		Assertions.assertTrue(listeOuvragesAux.get(0).getReservations() == 0);
+		Assertions.assertTrue(listeOuvragesAux.get(1).getReservations() == 0);
+		Assertions.assertTrue(listeOuvragesAux.get(2).getReservations() == 0);
+	}
+	
+	
+	@Test
+	public void setReservationListNotNullTest() {
 		
+		List<Ouvrage> ouvrages = new ArrayList<>();
+		o1 = new Ouvrage(1, "titre1", "nom1", "prenom1", "edition1", "genre1", exemplaires1_o1);
+		o2 = new Ouvrage(2, "titre2", "nom2", "prenom2", "edition2", "genre2", exemplaires2_o2);
+		o3 = new Ouvrage(3, "titre3", "nom3", "prenom3", "edition3", "genre3", exemplaires3_o3);
 		
+		ouvrages.add(o1);
+		ouvrages.add(o2);
+		ouvrages.add(o3);
+		
+		List<Reservation> reservations_o1 = new ArrayList<>();
+		List<Reservation> reservations_o2 = new ArrayList<>();
+		List<Reservation> reservations_o3 = new ArrayList<>();
+		
+		Reservation r1_o1 = new Reservation();
+		reservations_o1.add(r1_o1);
+		
+		Reservation r1_o2 = new Reservation();
+		reservations_o2.add(r1_o2);
+		
+		Reservation r2_o2 = new Reservation();
+		reservations_o2.add(r2_o2);
+		
+		Reservation r1_o3 = new Reservation();
+		reservations_o3.add(r1_o3);
+		
+		Reservation r2_o3 = new Reservation();
+		reservations_o3.add(r2_o3);
+		
+		Reservation r3_o3 = new Reservation();
+		reservations_o3.add(r3_o3);
+		
+		List<OuvrageAux> listeOuvragesAux = new ArrayList<>();
+		for(Ouvrage o: ouvrages) {
+			
+			OuvrageAux oAux = new OuvrageAux(o);
+			listeOuvragesAux.add(oAux);
+			
+		}
+
+		when(reservationService.obtenirListeReservationsParOuvrage(1)).thenReturn(reservations_o1);
+		when(reservationService.obtenirListeReservationsParOuvrage(2)).thenReturn(reservations_o2);
+		when(reservationService.obtenirListeReservationsParOuvrage(3)).thenReturn(reservations_o3);
+		ouvragesController.setReservations(listeOuvragesAux);
+		Assertions.assertTrue(listeOuvragesAux.get(0).getReservations() == 1);
+		Assertions.assertTrue(listeOuvragesAux.get(1).getReservations() == 2);
+		Assertions.assertTrue(listeOuvragesAux.get(2).getReservations() == 3);
 		
 	}
 }
